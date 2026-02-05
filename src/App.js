72cda2814644e48e1147086e21d6edd2aaf3bc20
Sehee <choisehee050616@gmail.com>
2026-02-05 14:16:05 +0900

    init : RoomCreatePage 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TestPage from "./pages/TestPage/TestPage";
+{/*import TestPage from "./pages/TestPage/TestPage";
 import MatchPage from "./pages/MatchPage/MatchPage";
-import MismatchPage from "./pages/MismatchPage/MismatchPage";
+import MismatchPage from "./pages/MismatchPage/MismatchPage"; */}
+import RoomCreatePage from "./pages/RoomCreatePage/RoomCreatePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestPage />} />
-        <Route path="/match" element={<MatchPage />} />
-        <Route path="/mismatch" element={<MismatchPage />} />
+        <Route path="/" element={<RoomCreatePage />} />
+        {/*<Route path="/match" element={<MatchPage />} />
+        <Route path="/mismatch" element={<MismatchPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
