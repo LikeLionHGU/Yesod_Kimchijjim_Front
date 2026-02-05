4ec1abb5280246194b55b96b4f51e2eb89d1bcba
Sehee <choisehee050616@gmail.com>
2026-02-05 16:48:15 +0900

    add : BackButton 컴포넌트화

---
+++ src/components/common/BackButton.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { Colors } from "../../styles/colors";
+import  { useNavigate } from "react-router-dom";
+
+
+const BackButtonStyle = styled.button`
+    position: absolute;
+    top: 90px;
+    left: 157px; 
+    border-radius: 11px;
+    background: ${Colors.white};
+    box-shadow: 0 0 10px 0 ${Colors.boxShadowBlack};
+    display: flex;
+    width: 39px;
+    height: 39px;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    cursor: pointer;
+
+    &:hover{
+        opacity: 0.5;
+    }
+`;
+
+const GoBackPage = () => {
+    
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1);
+    };
+
+    return(
+        <BackButtonStyle onClick={handleGoBack}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none">
+                <path d="M9.41406 1.41406L2.82812 8L9.41406 14.5859L8 16L0 8L8 0L9.41406 1.41406Z" fill="#A2A2A2" />
+            </svg>
+        </BackButtonStyle>
+    )
+};
+
+export default GoBackPage;
