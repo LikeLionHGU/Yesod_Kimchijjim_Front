87f9c6b2c6891e273dbea683dad2b012473ea32d
Sehee <choisehee050616@gmail.com>
2026-02-06 17:34:38 +0900

    init: RoomJoinPage 생성, 흰 박스 만들기

--- src/pages/RoomJoinPage/RoomJoinPage.js
+++ src/pages/RoomJoinPage/RoomJoinPage.js
@@ -9,6 +9,36 @@ import TitleIcon from "../../assets/Ellipse 5.svg";
 
 const RoomJoinPage = () => {
     const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1);
+    };
+
+    const [userName, setUserName] = useState("");
+    const [isUserNameError, setIsUserNameError] = useState(false);
+
+    const handleNameChange = (e) => {
+        const value = e.target.values;
+        setUserName(value);
+
+        const regex = /^[가-힣a-zA-Z]+$/;
+
+        if(value.length === 0) {
+            setIsUserNameError(false);
+            return;
+        }
+
+        if(value.length < 2 || value.length > 10 || !regex.test(value)) {
+            setIsUserNameError(true);
+        } else {
+            setIsUserNameError(false);
+        }
+    };
+
+    const isActive = 
+        !isUserNameError && userName.length >= 2;
+
+
     return(
         <PageContainer>
             <GoBackPage/>
@@ -17,6 +47,9 @@ const RoomJoinPage = () => {
                 titleText={"방 들어가기"}
                 subTitleText={"우리 방에 들어가요"}
             />
+            <Card></Card>
+
+
         </PageContainer>
     )
 }
@@ -35,4 +68,17 @@ const PageContainer = styled.div`
     padding-bottom: 269px;
     position: relative;
     box-sizing: border-box;
+`;
+
+const Card = styled.div`
+    border-radius: 15px;
+    background: ${Colors.white};
+    box-shadow: 0 0 15px 0 rgba(163, 163, 253, 0.30);
+    width: 555px;
+    height: 192px;
+    box-sizing: border-box;
+    padding: 49px 95px;
+    gap: 10px;
+    display: flex;
+    flex-direction: column;
 `;
