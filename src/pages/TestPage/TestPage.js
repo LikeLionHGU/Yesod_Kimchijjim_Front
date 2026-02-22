1d0ad2e089062474b99136e88b02d32e83e12c60
Sehee <choisehee050616@gmail.com>
2026-02-22 20:23:07 +0900

    style: landingPage 수정

--- src/pages/TestPage/TestPage.js
+++ src/pages/TestPage/TestPage.js
@@ -352,9 +352,11 @@ function TestPage() {
         return;
       }
 
+      const serverData = Array.isArray(res) ? res : (res?.data || []);
+
       const nextPayload = {
         ...payload,
-        data: res?.data || [],
+        data: serverData,
         amIHost: res?.amIHost,
       };
 
